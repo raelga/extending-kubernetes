4a4be0a90b50bb1ec85ae09dfd0f5a1c2ca6ce55
rael <rael@rael.io>
2019-09-01 16:44:54 +0200

    Restore non-spot instance from 6e530ac

---
+++ infra/terraform/modules/aws/ec2/instance/main.tf
@@ -0,0 +1,88 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_security_group" "instance-sg" {
+  vpc_id = "${var.vpc}"
+
+  ingress {
+    protocol  = "TCP"
+    from_port = 22
+    to_port   = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol  = "TCP"
+    from_port = 80
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol  = "TCP"
+    from_port = 443
+    to_port   = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol  = "TCP"
+    from_port = 8080
+    to_port   = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.name}-sg"
+    account = "talks"
+  }
+}
+
+resource "aws_instance" "instance" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "${var.instance_type}"
+  subnet_id     = "${var.subnet}"
+  vpc_security_group_ids = [ "${aws_security_group.instance-sg.id}" ]
+
+  user_data = <<EOF
+#!/bin/bash
+# User configuration
+usermod -l ${var.system_user} -d /home/${var.system_user} -m ${var.system_default_user} && groupmod -n ${var.system_user} ${var.system_default_user};
+echo "${var.system_user} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/90-cloud-init-users
+curl -sq https://github.com/${var.github_user}.keys | tee -a /home/${var.system_user}/.ssh/authorized_keys
+# Package installation
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository -y ppa:longsleep/golang-backports
+sudo apt-get install -y docker-ce git golang-go
+sudo usermod -aG docker ${var.system_user}
+EOF
+
+  tags = {
+    Name    = "${var.name}"
+    account = "talks"
+  }
+} 
---
+++ infra/terraform/modules/aws/ec2/instance/variables.tf
@@ -0,0 +1,41 @@
+variable "name" {
+  description = "EC2 instance name"
+  type        = "string"
+}
+
+variable "region" {
+  description = "EC2 region"
+  type        = "string"
+  default     = "eu-west-1"
+}
+variable "vpc" {
+  description = "EC2 VPC"
+  type        = "string"
+}
+variable "subnet" {
+  description = "EC2 VPC Subnet"
+  type        = "string"
+}
+
+variable "instance_type" {
+  description = "EC2 Instance type"
+  type        = "string"
+  default     = "t3a.large"
+}
+
+variable "system_default_user" {
+  description = "EC2 instance default user"
+  type        = "string"
+  default     = "ubuntu"
+}
+
+variable "system_user" {
+  description = "EC2 instance user"
+  type        = "string"
+  default     = "rael"
+}
+variable "github_user" {
+  description = "GitHub user, to retrieve the public ssh keys"
+  type        = "string"
+  default     = "raelga"
+}
