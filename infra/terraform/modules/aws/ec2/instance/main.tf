89d910e57915944135978998076d219cc03f99bc
rael <rael@rael.io>
2019-09-01 15:49:21 +0200

    Open default app ports

--- infra/terraform/modules/aws/ec2/instance/main.tf
+++ infra/terraform/modules/aws/ec2/instance/main.tf
@@ -20,9 +20,29 @@ resource "aws_security_group" "instance-sg" {
 
   ingress {
     protocol  = "TCP"
-    self      = true
     from_port = 22
     to_port   = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol  = "TCP"
+    from_port = 80
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol  = "TCP"
+    from_port = 443
+    to_port   = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol  = "TCP"
+    from_port = 8080
+    to_port   = 8080
     cidr_blocks = ["0.0.0.0/0"]
   }
 
