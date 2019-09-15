fba43b9e53d245cc3e2464298d1e5cafcfc1e42c
lominorama <roivazquez@gmail.com>
2019-09-15 17:57:59 +0200

    Use talks.cloudnative.barcelona dns zone

--- infra/terraform/resources/gcp-gke/main.tf
+++ infra/terraform/resources/gcp-gke/main.tf
@@ -1,6 +1,6 @@
 locals {
   cluster_name = "extending-k8s"
-  managed_zone = "gcloud.lominorama.com"
+  manage_zone  = "talks.cloudnative.barcelona"
 }
 
 module "gke" {
@@ -8,24 +8,18 @@ module "gke" {
   name   = "${local.cluster_name}"
 }
 
-module "managed_zone" {
-  source   = "../../modules/gcp/dns/managed_zone"
-  name     = "gcloud-lominorama-com"
-  dns_name = "gcloud.lominorama.com."
-}
-
 # module "elasticsearch_endpoint" {
-#   source            = "../../modules/gcp/dns/endpoint"
-#   name              = "elasticsearch"
-#   cluster_name      = "${local.cluster_name}"
-#   managed_zone      = "${local.managed_zone}"
-#   svc_name          = "elasticsearch-es-http"
+#   source       = "../../modules/gcp/dns/endpoint"
+#   name         = "elasticsearch"
+#   cluster_name = "${local.cluster_name}"
+#   dns_zone     = "${local.dns_zone}"
+#   svc_name     = "elasticsearch-es-http"
 # }
 
 # module "kibana_endpoint" {
-#   source            = "../../modules/gcp/dns/endpoint"
-#   name              = "kibana"
-#   cluster_name      = "${local.cluster_name}"
-#   managed_zone      = "${local.managed_zone}"
-#   svc_name          = "kibana-kb-http"
+#   source       = "../../modules/gcp/dns/endpoint"
+#   name         = "kibana"
+#   cluster_name = "${local.cluster_name}"
+#   dns_zone     = "${local.dns_zone}"
+#   svc_name     = "kibana-kb-http"
 # }
